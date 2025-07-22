c209e7fe6f6587e665a4256fab2f6c6e9fc0c8f2
dalps <git@dalp.sh>
2025-07-23 01:05:53 +0200

    MDN's WebGL tutorial until Create the square plane
    https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context

---
+++ justfile
@@ -0,0 +1,2 @@
+default:
+    pnpm run dev
---
+++ src/webgl-demo.ts
@@ -0,0 +1,89 @@
+main();
+
+function main() {
+  const canvas = document.getElementById("gl-canvas") as HTMLCanvasElement;
+
+  const gl = canvas.getContext("webgl");
+
+  if (gl === null) {
+    alert("WebGL not supported. Upgrade your gear.");
+    return;
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  const vsSource = /*glsl*/ `
+attribute vec4 aVertexPosition;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main() {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
+  `;
+
+  const fsSource = /*glsl*/ `
+void main(){
+gl_FragColor = vec4(1.0,1.0,1.0,1.0);
+}`;
+
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(
+        shaderProgram,
+        "uProjectionMatrix"
+      ),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+    },
+  };
+}
+
+function initShaderProgram(
+  gl: WebGL2RenderingContext,
+  vsSource: string,
+  fsSource: string
+) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert(
+      `Something went wrong during shader program initialization: ${gl.getProgramInfoLog(
+        shaderProgram
+      )}`
+    );
+
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+function loadShader(gl: WebGL2RenderingContext, type: number, source: string) {
+  const shader = gl.createShader(type);
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(
+      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`
+    );
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
